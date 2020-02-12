b7f98ffc5059c5585ff52c4b15385d605e008f82
zhenik <nikita.zhevnitskiy@gmail.com>
2020-02-12 20:55:50 +0100

    added authorization role

--- src/components/dashboard/Profile.vue
+++ src/components/dashboard/Profile.vue
@@ -3,8 +3,10 @@
     <h2>Profile</h2>
     <div class="text-md-center" v-if="user">
       <h1 class="green--text">Login Success</h1>
-      <h4 class="headline mb-0"><b class="red--text">Name :</b> {{ user.name }}</h4>
+      <h3 class="headline mb-0"><b class="red--text">Name :</b> {{ user.name }}</h3>
+      <h3 class="headline mb-0"><b class="red--text">Name :</b> {{ role }}</h3>
       <h4 class="headline mb-0"><b class="red--text">Email :</b> {{ user.email }}</h4>
+      <p class="headline mb-0"><b class="red--text">Email :</b> {{ user }}</p>
     </div>
   </div>
 
@@ -15,6 +17,9 @@
     computed: {
       user() {
         return this.$store.getters.user
+      },
+      role() {
+        return this.$store.getters.userRole
       }
     }
   }
--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,12 +1,17 @@
 import * as firebase from 'firebase'
+import { UsersService } from "../../api";
 
 export default {
   state: {
-    user: null
+    user: null,
+    role: null
   },
   mutations: {
     setUser (state, payload) {
       state.user = payload
+    },
+    setRole (state, role) {
+      state.role = role
     }
   },
   actions: {
@@ -39,7 +44,6 @@ export default {
       firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
           .then(
               response => {
-                commit('setLoading', false);
                 const newUser = {
                   id: response.user.uid,
                   name: response.user.displayName,
@@ -47,9 +51,27 @@ export default {
                   photoUrl: response.user.photoURL,
                   token: response.user.getIdToken()
                 };
-                commit('setUser', newUser)
+                commit('setUser', newUser);
+                return newUser.email;
               }
           )
+          .then( email => {
+            // console.log("Here "+ JSON.stringify(email))
+            UsersService.getUser(email)
+                .then(
+                    response => {
+                      console.log("My role is "+ response.data.role);
+                      commit('setLoading', false);
+                      commit('setRole', response.data.role)
+                    })
+                .catch(
+                    error => {
+                      commit('setLoading', false);
+                      commit('setError', error);
+                      console.log(error)
+                    }
+                );
+          })
           .catch(
               error => {
                 commit('setLoading', false);
@@ -58,14 +80,35 @@ export default {
               }
           )
     },
+    // authorizeUser({commit}, email) {
+    //   commit('setLoading', true);
+    //   commit('clearError');
+    //   UsersService.getUser(email)
+    //       .then(
+    //           response => {
+    //           commit('setLoading', false);
+    //           commit('setRole', response.role)
+    //       })
+    //       .catch(
+    //           error => {
+    //             commit('setLoading', false);
+    //             commit('setError', error);
+    //             console.log(error)
+    //           }
+    //       );
+    // },
     logout ({commit}) {
       firebase.auth().signOut();
-      commit('setUser', null)
+      commit('setUser', null);
+      commit('setRole', null);
     }
   },
   getters: {
     user (state) {
       return state.user
+    },
+    userRole (state) {
+      return state.role
     }
   }
 }
