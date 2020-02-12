8efdac46801b0bf9ff1662ac743b1e68a4d4ca5c
zhenik <nikita.zhevnitskiy@gmail.com>
2020-02-12 21:40:37 +0100

    refactored authorization

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -19,18 +19,7 @@ export default {
       commit('setLoading', true);
       commit('clearError');
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          response => {
-            commit('setLoading', false);
-            const newUser = {
-              id: response.user.uid,
-              name: response.user.displayName,
-              email: response.user.email,
-              photoUrl: response.user.photoURL
-            };
-            commit('setUser', newUser)
-          }
-        )
+        // no than, we are not going to auth auto
         .catch(
           error => {
             commit('setLoading', false);
@@ -56,11 +45,9 @@ export default {
               }
           )
           .then( email => {
-            // console.log("Here "+ JSON.stringify(email))
             UsersService.getUser(email)
                 .then(
                     response => {
-                      console.log("My role is "+ response.data.role);
                       commit('setLoading', false);
                       commit('setRole', response.data.role)
                     })
@@ -69,6 +56,10 @@ export default {
                       commit('setLoading', false);
                       commit('setError', error);
                       console.log(error)
+                      // logout firebase, if authorization failed
+                      firebase.auth().signOut();
+                      commit('setUser', null);
+                      commit('setRole', null);
                     }
                 );
           })
@@ -80,23 +71,6 @@ export default {
               }
           )
     },
-    // authorizeUser({commit}, email) {
-    //   commit('setLoading', true);
-    //   commit('clearError');
-    //   UsersService.getUser(email)
-    //       .then(
-    //           response => {
-    //           commit('setLoading', false);
-    //           commit('setRole', response.role)
-    //       })
-    //       .catch(
-    //           error => {
-    //             commit('setLoading', false);
-    //             commit('setError', error);
-    //             console.log(error)
-    //           }
-    //       );
-    // },
     logout ({commit}) {
       firebase.auth().signOut();
       commit('setUser', null);
