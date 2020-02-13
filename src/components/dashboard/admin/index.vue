c4e20fc90b5959109b0d04cf5606b6ea23803bb5
zhenik <nikita.zhevnitskiy@gmail.com>
2020-02-13 21:21:39 +0100

    stubs

---
+++ src/components/dashboard/admin/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Admin dash board</h2>
+  </div>
+</template>
--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="dashboard">
     <h2>This is dashboard</h2>
+    <div v-if="isAdmin"></div>
+    <admin-dashboard v-if="isAdmin"></admin-dashboard>
+    <user-dashboard v-else></user-dashboard>
   </div>
 </template>
+
+<script>
+  import { mapGetters } from "vuex";
+  import AdminDashboard from "./admin/index"
+  import UserDashboard from "./user/index"
+
+  export default {
+    computed: {
+      ...mapGetters({
+        profile: "profile",
+        isAdmin: "isAdmin"
+      }),
+    },
+    components: {
+      userDashboard: UserDashboard,
+      adminDashboard: AdminDashboard
+    },
+  }
+</script>
