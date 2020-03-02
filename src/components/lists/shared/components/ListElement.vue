1ded4ca71dc31f7003697efba68ab8efc2e3b3cb
zhenik <nikita.zhevnitskiy@gmail.com>
2020-03-02 22:06:09 +0100

    added radio btns

--- src/components/lists/shared/components/ListElement.vue
+++ src/components/lists/shared/components/ListElement.vue
@@ -2,39 +2,80 @@
   <div>
     <h3 v-if="isAdmin">Edit list</h3>
     <h2>List element</h2>
-    {{ this.getCurrentList }}
+    {{ list }}
+
+    <ul class="list-group">
+      <li
+          v-for="question in list.questions"
+          v-bind:question="question"
+          :key="question.id"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+
+        {{question.text}}
+
+        <div class="btn-group mr-2 btn-group-toggle" data-toggle="buttons">
+          <label class="btn btn-secondary">
+            <input type="radio"
+                   :value="question.answer"
+                   v-model="picked"
+                   > Yes
+          </label>
+          <label class="btn btn-secondary">
+            <input type="radio"
+                   :value="question.answer"
+                   v-model="picked"
+            > No
+          </label>
+        </div>
+
+        You have selected : {{question.answer}}
+
+      </li>
+
+    </ul>
+
+
   </div>
 
 </template>
 
 <script>
-  import {mapActions, mapGetters} from "vuex";
+  import {mapGetters} from "vuex";
   import { store } from "@/store";
 
   export default {
+    name: "ListElement",
     props: {
       slug: {
         type: String,
         required: true
       },
     },
-    beforeRouteEnter(to, from, next) {
-      Promise.all([
-        store.dispatch('fetchCurrentList', to.params.slug),
-      ]).then(() => {
-        next();
-      });
+    data() {
+      return {
+        list: {
+          id: '',
+          updatedAt: '',
+          createdAt: '',
+          assignedToEmail: '',
+          assignedDate: '',
+          delivered: '',
+          deadline: '',
+          questions: [],
+        },
+        picked: ''
+      }
     },
-    beforeRouteLeave(to, from, next) {
-      Promise.all([
-        this.cleanCurrentList
-      ]).then(() => {
-        next();
-      });
+    mounted() {
+      let currentList = this.$store.getters.getCurrentList
+      this.list = currentList
+    },
+    async beforeRouteEnter(to, from, next) {
+      await store.dispatch('fetchCurrentList', to.params.slug)
+      next();
     },
     computed: {
-      ...mapGetters(["getCurrentList", "isAdmin"]),
-      ...mapActions(["cleanCurrentList"])
+      ...mapGetters(["getCurrentList", "getCurrentQuestions", "isAdmin"]),
     },
   }
 </script>
