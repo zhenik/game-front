94e426c42df88b45e82d68a767797a2bfd329080
zhenik <nikita.zhevnitskiy@gmail.com>
2020-03-27 18:53:06 +0100

    rm prototyping

--- src/css-prototype/trash/AdminWhatever.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h2>Admin header</h2>
-    <a to="#">New List</a>
-  </div>
-</template>
--- src/css-prototype/trash/UserWhatever.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h2>User header</h2>
-    <a to="#">New List</a>
-  </div>
-</template>
