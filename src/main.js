b5ea82ebc3cbb0097cc4bb38323bd05ddd8d05ec
zhenik <nikita.zhevnitskiy@gmail.com>
2020-04-02 20:25:20 +0200

    extract creds to env

--- src/main.js
+++ src/main.js
@@ -1,27 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
 import * as firebase from 'firebase'
-import { config } from './firebase.js'
 import router from './router/index'
 import { store } from './store'
 import ApiService from "./api/index";
 
 Vue.config.productionTip = false;
+
+let config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
 ApiService.init();
+firebase.initializeApp(config);
+// todo: https://blog.logrocket.com/vue-firebase-authentication/
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     // this.$store.dispatch('/profile', user)
+//   } else {
+//     this.$router.push('/signin');
+//   }
+// })
+
+// firebase.onAuthStateChanged((u) => {
+//   if (u == null) this.isLoggedIn = false;
+//   else this.isLoggedIn = true;
+// });
 
 new Vue({
   // el: '#app',
   router,
   store,
-  created () {
-    firebase.initializeApp(config)
-    // firebase.auth().onAuthStateChanged((user) => {
-    //   if (user) {
-    //     // this.$store.dispatch('/profile', user)
-    //   } else {
-    //     this.$router.push('/signin');
-    //   }
-    // })
-  },
   render: h => h(App)
 }).$mount('#app');
