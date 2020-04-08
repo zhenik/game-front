20f7fd5b5adaa15ffdd005bacff61905eb984e31
zhenik <nikita.zhevnitskiy@gmail.com>
2020-04-08 11:13:49 +0200

    add loopback

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,3 @@
 // export const API_URL = process.env.VUE_APP_API_URL;
-export const API_URL = "http://0.0.0.0:3001";
-// export const API_URL = "http://backend-ocn:8080";
+export const API_URL = "/api/v1/";
 export default API_URL;
